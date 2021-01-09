77f9050c685ef8503d1a0dda752c23d1d2976b12
Thomas Heigl <thomasheigl2@gmail.com>
2021-01-09 23:02:33 +0100

    Added the DiaryController

---
+++ Api/Controllers/Diary/DiaryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Api.Controllers.Diary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Diary
+{
+    /// <summary>
+    /// Handles interactions woth the diary of the current user
+    /// </summary>
+    [ApiController]
+    [Route("/api/diary")]
+    public class DiaryController : ControllerBase
+    {
+        public DiaryController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<DiaryEntryViewModel> GetAll()
+        {
+            return new List<DiaryEntryViewModel>
+            {
+                new DiaryEntryViewModel
+                {
+                    EventAt = DateTime.Now,
+                    Description = "Testentry"
+                }
+            };
+        }
+    }
+}
---
+++ Api/Controllers/Diary/Models/DiaryEntryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Api.Controllers.Diary.Models
+{
+    /// <summary>
+    /// The view model for diary entries
+    /// </summary>
+    public class DiaryEntryViewModel
+    {
+        /// <summary>
+        /// When the described event happened
+        /// </summary>
+        public DateTime EventAt { get; set; }
+
+        /// <summary>
+        /// What happened at this event
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
