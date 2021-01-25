653d18a4c3baf06def193d58d7d2838d760497c2
Thomas Heigl <info@thomasheigl.com>
2021-01-25 22:07:55 +0100

    Added actions to un-/archive or delete diary entries.

--- Api/Database/Models/DiaryEntry.cs
+++ Api/Database/Models/DiaryEntry.cs
@@ -20,6 +20,11 @@ namespace Api.Database.Models
         public string Description { get; set; }
 
         /// <summary>
+        /// Whether this entry is archived
+        /// </summary>
+        public bool IsArchived { get; set; }
+
+        /// <summary>
         /// The user this entry belongs to
         /// </summary>
         [ForeignKey(nameof(UserId))]
