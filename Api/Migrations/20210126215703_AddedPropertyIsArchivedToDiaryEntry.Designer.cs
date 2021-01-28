4bf282dd8be62d232d87d4372b67827e75c907cb
Thomas Heigl <info@thomasheigl.com>
2021-01-28 22:47:17 +0100

    Added migration for the new IsArchived prop

--- Api/Migrations/20210126215703_AddedPropertyIsArchivedToDiaryEntry.Designer.cs
+++ Api/Migrations/20210126215703_AddedPropertyIsArchivedToDiaryEntry.Designer.cs
@@ -4,14 +4,16 @@ using Api.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Api.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210126215703_AddedPropertyIsArchivedToDiaryEntry")]
+    partial class AddedPropertyIsArchivedToDiaryEntry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api/Migrations/20210126215703_AddedPropertyIsArchivedToDiaryEntry.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class AddedPropertyIsArchivedToDiaryEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "DiaryEntries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "DiaryEntries");
+        }
+    }
+}
