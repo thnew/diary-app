16667dc945ae2c7df252b70c57160e81a980e6d2
Thomas Heigl <info@thomasheigl.com>
2021-09-22 14:55:44 +0200

    Added web project for web user interface

---
+++ Web/ClientApp/src/main.ts
@@ -0,0 +1,23 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+const providers = [
+  {
+    provide: 'BASE_URL',
+    useFactory: () => {
+      return document.getElementsByTagName('base')[0].href
+    },
+    deps: []
+  }
+];
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic(providers)
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err));
